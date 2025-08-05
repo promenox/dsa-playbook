cefc13c94ad2c8d6987b5ebac39d42c0275bf4f6
Michael Kimber <mail2kimber@gmail.com>
2025-08-05 07:54:13 +0200

    adding some ideas for sorting algos

--- Python/bubble_sort.py
+++ Python/bubble_sort.py
@@ -6,3 +6,9 @@ test_array = [5, 1, 4, 2, 3]
 # solution arrays
 ascd_array = [1, 2, 3, 4, 5]
 dscd_array = [5, 4, 3, 2, 1]
+
+# working on imp.
+def bubble_sort(nums):
+    for i in len(nums):
+        for j in nums:
+            break
