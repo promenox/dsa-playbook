ff22e053808f328c657aa88496f7cc2736d94175
Michael Kimber <mail2kimber@gmail.com>
2025-08-20 04:28:20 -0700

    more linting fixes

--- Python/val_array.py
+++ Python/val_array.py
@@ -1,4 +1,4 @@
-# finding the largest and smallest items from an array and returning those values
+# finding the largest and smallest items from an array and returning those vals
 
 # -- min imp --
 
@@ -8,14 +8,16 @@ def min_val_built(nums):
         min_ = min(min_, num)
     return min_
 
+
 def min_val_cond(nums):
     min_ = 0
     for num in nums:
-        if(num < min_):
+        if (num < min_):
             min_ = num
     return min_
 
 # =- max imp --
+
 
 def max_val_built(nums):
     max_ = 0
@@ -23,9 +25,10 @@ def max_val_built(nums):
         max_ = max(max_, num)
     return max_
 
+
 def max_val_cond(nums):
     max_ = 0
     for num in nums:
-        if(num > max_):
+        if (num > max_):
             max_ = num
     return max_
