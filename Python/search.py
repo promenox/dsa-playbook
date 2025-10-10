eee342c0f4501eebb1de629e458a5928e3a475af
Michael Kimber <mail2kimber@gmail.com>
2025-10-09 23:35:13 -0700

    added dfs search method

--- Python/search.py
+++ Python/search.py
@@ -16,7 +16,7 @@ def reconstruct_path(parent, start, goal):
 
     return path[::-1]
 
-# BFS search
+# bfs search
 def bfs_search(tree, start, goal):
 
     closed_set = []
@@ -47,6 +47,35 @@ def bfs_search(tree, start, goal):
     
     return reconstruct_path(parent, start, goal)
 
-print(bfs_search(tree, 'S', 'G'))
+# dfs search
+def dfs_search(tree, start, goal):
+    closed_set = []
+    open_set = Stack([start])
+    parent = {'S': None}
 
-# DFS search
+    idx = 0
+
+    while True:
+        
+        print(f"idx:{idx}\nclosed:\t{closed_set}\nopen:\t{open_set.stack}\n")
+
+        if (open_set.isEmpty()):
+            break
+        
+        node = open_set.pop()
+        closed_set.append(node)
+
+        if node == 'G':
+            break
+
+        # we need to reverse the left of child nodes to get the l -> r convention
+        for child in reversed(tree[node]):
+            parent[child] = node
+            open_set.push(child)
+        
+        idx += 1 
+    
+    return reconstruct_path(parent, start, goal)
+
+print(bfs_search(tree, 'S', 'G'))
+print(dfs_search(tree, 'S', 'G'))
