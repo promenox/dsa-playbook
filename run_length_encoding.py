a0f78b4ed2d83ff72bc28901177dae27198dd310
Michael Kimber <mail2kimber@gmail.com>
2025-12-29 10:20:04 -0800

    finished rl encoding file

--- run_length_encoding.py
+++ run_length_encoding.py
@@ -13,13 +13,28 @@ def rl_encode(s):
             result += str(count) + s[i - 1]
             count = 1
     
-    # handle last run
+    # handle appendage on last run
     result += str(count) + s[i - 1]
 
     return result
 
+def rl_decode(s):
+    if not s:
+        return ""
+    
+    result = ""
+
+    # assumes pairwise str [digit][char] 
+    for i in range(len(s) // 2):
+        # expands pairwise in explicit format [digit][char]
+        result += ((s[(i * 2) + 1]) * int(s[(i * 2)]))
+
+    return result
+
+# encoding test
 print(rl_encode("aaaaaabbbb"))
 print(rl_encode("aabbababab"))
 
-# def rl_decode(string):
-#     pass
+# decoding test
+print(rl_decode("6a4b"))
+print(rl_decode("2a2b1a1b1a1b1a1b"))
