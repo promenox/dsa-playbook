07ec16e73d988bcff90900a346702ed6ca1843ff
Michael Kimber <mail2kimber@gmail.com>
2025-12-30 22:03:00 -0500

    added base obj structure code

---
+++ linked_list.py
@@ -0,0 +1,32 @@
+class MyListNode:
+    def __init__(self, id):
+        self.id = id
+    
+    def get_id(self):
+        print(self.id)
+
+obj = MyListNode(22)
+obj.get_id()
+
+
+# class LinkedList():
+#     def search():
+#         pass
+    
+#     def insert():
+#         pass
+
+#     def delete():
+#         pass
+
+#     def minimum():
+#         pass
+
+#     def maximum():
+#         pass
+
+#     def sucessor():
+#         pass 
+
+#     def predecessor():
+#         pass
